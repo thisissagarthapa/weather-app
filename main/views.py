60e46b9f187e829d1bcb163667897a045f9590ed
Sagar Thapa <kingstonboysagar@gmail.com>
2024-08-29 16:54:29 +0545

    add bar chart logics

--- main/views.py
+++ main/views.py
@@ -1,35 +1,58 @@
+import requests
 from django.shortcuts import render
 from django.contrib import messages
-import requests
-# https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=6d2e400206202418fd767656b3a8a1d7
-# https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-# apikey='6d2e400206202418fd767656b3a8a1d7'
+import matplotlib.pyplot as plt
+import io
+import base64
 
-# Create your views here.
+def create_chart(temperature, humidity, wind_speed):
+    # Create a bar chart
+    fig, ax = plt.subplots()
+    ax.bar(['Temperature', 'Humidity', 'Wind Speed'], [temperature, humidity, wind_speed])
+    ax.set_ylabel('Value')
+    ax.set_title('Weather Data')
 
+    # Save to a BytesIO object
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    chart_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+    buffer.close()
+    
+    return chart_base64
 
 def index(request):
     temperature = ''
     humidity = ''
     wind_speed = ''
     city = ''
+    chart = ''  # Initialize chart variable
 
     if 'city' in request.POST:
         city = request.POST['city']
-        api_key = '6d2e400206202418fd767656b3a8a1d7'
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    else:
+        city = "kathmandu"
         
-        # Make HTTP request to OpenWeatherMap API
+    api_key = '6d2e400206202418fd767656b3a8a1d7'
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    
+    try:
         response = requests.get(url)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            data = response.json()
-            temperature = data["main"]["temp"]
-            humidity = data["main"]["humidity"]
-            wind_speed = data["wind"]["speed"]
-        else:
-           messages.success(request,'Incorrect city name ')
-        
+        data = response.json()
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+        chart = create_chart(temperature, humidity, wind_speed)  # Generate chart
+    except:
+        temperature = "0"
+        humidity = "0"
+        wind_speed = "0"
+        messages.error(request, 'Incorrect city name or API error')
 
-    return render(request, 'weather.html', {'temperature': temperature, 'humidity': humidity, 'wind_speed': wind_speed, 'city': city})
+    return render(request, 'weather.html', {
+        'temperature': temperature,
+        'humidity': humidity,
+        'wind_speed': wind_speed,
+        'city': city,
+        'chart': chart
+    })
